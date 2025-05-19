f265c963da9be596aa08ff493d5969c360a8a4dc
Lenovo <Lenovo@LAPTOP-PT2G7R7M>
2025-05-20 00:50:15 +0700

    pekan 4

--- PrakSD24_1_2411533016/src/Pekan4/ContohQueue2.java
+++ PrakSD24_1_2411533016/src/Pekan4/ContohQueue2.java
@@ -4,16 +4,21 @@ import java.util.Queue;
 
 public class ContohQueue2 {
 	public static void main(String[] args) {
+		
 		Queue<Integer> q = new LinkedList<>();
 		// TAMBAH ELEMEN (0, 1, 2, 3, 4, 5) KE ANTRIAN
 		for (int i = 0; i < 6; i++)
 			q.add(i);
+		
 		// menampilkan isi antrian.
 		System.out.println("Elemen Antrian " + q);
+		
 		// UNTUK MENGHAPUS KEPALA ANTRIAN
 		int hapus = q.remove();
 		System.out.println("Hapus elemen = " + hapus);
+		
 		System.out.println(q);
+		
 		// untuk melihat antrian terdepan
 		int depan = q.peek();
 		System.out.println("Kepala Antrian = " + depan);
--- PrakSD24_1_2411533016/src/Pekan4/InputQueue.java
+++ PrakSD24_1_2411533016/src/Pekan4/InputQueue.java
@@ -4,6 +4,7 @@ public class InputQueue {
 int front, rear, size;
 int capacity;
 int array[];
+
 public InputQueue(int capacity)
 {
 this.capacity = capacity;
@@ -11,17 +12,16 @@ front = this.size = 0;
 rear = capacity - 1;
 array = new int[this.capacity];
 }
-boolean isFull (InputQueue queue)
-{
+
+boolean isFull (InputQueue queue) {
 return (queue.size == queue.capacity);
 }
 
-boolean isEmpty(InputQueue queue)
-{
+boolean isEmpty(InputQueue queue) {
 return (queue.size == 0);
 }
-void enqueue(int item)
-{
+
+void enqueue(int item) {
 if (isFull(this))
 return;
 this.rear = (this.rear + 1) %
@@ -31,29 +31,27 @@ this.size = this.size + 1;
 System.out.println(item + " enqueued to queue");
 }
 
-int dequeue()
-{
-if (isEmpty(this))
-return Integer.MIN_VALUE;
+int dequeue() {
+	if (isEmpty(this))
+		return Integer.MIN_VALUE;
 
-int item = this.array[this.front];
-this.front = (this.front + 1) %
-this.capacity;
-this.size = this.size - 1;
-return item;
+	int item = this.array[this.front];
+	this.front = (this.front + 1) %
+	this.capacity;
+	this.size = this.size - 1;
+	return item;
+	}
+
+int front() {
+	if (isEmpty(this))
+		return Integer.MIN_VALUE;
+
+	return this.array[this.front];
 }
-int front()
-{
-if (isEmpty(this))
-return Integer.MIN_VALUE;
+int rear () {
+	if (isEmpty(this))
+		return Integer.MIN_VALUE;
 
-return this.array[this.front];
-}
-int rear ()
-{
-if (isEmpty(this))
-	return Integer.MIN_VALUE;
-
-return this.array[this.rear];
+	return this.array[this.rear];
 }
 }
